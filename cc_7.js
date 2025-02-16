7656ed4b633293750df6534bc11d4a9bb86bb02d
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-02-16 02:52:02 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -27,3 +27,30 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
 //Test cases - Values can be changed
 calculateHourlyWage(52000, 40); //Expected output: $25.00
 calculateHourlyWage(75000, 35); //Expected output: $41.21
+
+//Task 3 - Customer Loyalty Discount
+
+//Arrow function to calculate discount based on loyalty years
+const calculateLoyaltyDiscount = (amount, years) => {
+    //Declaring the discountRate variable
+    let discountRate;
+
+    //15% discount for 5 or more years
+    if (years >= 5) discountRate = 0.15;
+
+    //10% discount for 3 or more years
+    else if (years >= 3) discountRate = 0.1;
+
+    //5% discount for less than 3 years
+    else discountRate = 0.05;
+
+    //Calculating the discounted price
+    const discountedPrice = amount * (1 - discountRate);
+    
+    //Discounted price is logged to the console to two decimal places
+    console.log(`Discounted price: $${discountedPrice.toFixed(2)}`);
+};
+
+//Test cases - Values can be changed
+calculateLoyaltyDiscount(100, 6); //Expected output: $85.00
+calculateLoyaltyDiscount(200, 2); //Expected output: $190.00
