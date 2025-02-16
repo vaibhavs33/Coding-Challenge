7831eeb260641ee4b3b4dabe49eeb6c3d8bde95f
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-02-16 02:50:44 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -12,3 +12,18 @@ function calculateInvoice(subtotal, taxRate, discount) {
 //Test cases - Values can be changed
 calculateInvoice(100, 0.08, 5); //Expected output: $103.00
 calculateInvoice(500, 0.1, 20); //Expected output: $530.00
+
+//Task 2 - Employee Hourly Wage Calculation
+
+//Function expression that calculates the hourly wage based on the annual salary and average weekly work hours
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    //Dividing the salary by the total work hours in a year (52 weeks)
+    const hourlyWage = salary / (hoursPerWeek * 52);
+
+    //Hourly wage is logged to the console to two decimal places. 
+    console.log(`Hourly wage: $${hourlyWage.toFixed(2)}`);
+};
+
+//Test cases - Values can be changed
+calculateHourlyWage(52000, 40); //Expected output: $25.00
+calculateHourlyWage(75000, 35); //Expected output: $41.21
